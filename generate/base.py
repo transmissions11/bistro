529d4c654c016c1d94003d204fd9f77d19882585
t11s <greenfeatherhelp@gmail.com>
2023-08-08 17:51:00 -0700

    Update base.py

--- generate/base.py
+++ generate/base.py
@@ -179,18 +179,13 @@ def main(
 
 
 if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    torch.set_float32_matmul_precision("high")
     warnings.filterwarnings(
         # Triggered internally at ../aten/src/ATen/EmptyTensor.cpp:31
         "ignore",
         message="ComplexHalf support is experimental and many operators don't support it yet",
     )
-    warnings.filterwarnings(
-        "ignore",
-        message="UserWarning",
-    )
-
-    from jsonargparse import CLI
-
-    torch.set_float32_matmul_precision("high")
 
     CLI(main)
