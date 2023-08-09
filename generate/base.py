19efca8c8539b422b3b7ab338c2e899f413babd0
t11s <greenfeatherhelp@gmail.com>
2023-08-08 17:50:16 -0700

    Update base.py

--- generate/base.py
+++ generate/base.py
@@ -179,9 +179,6 @@ def main(
 
 
 if __name__ == "__main__":
-    from jsonargparse import CLI
-
-    torch.set_float32_matmul_precision("high")
     warnings.filterwarnings(
         # Triggered internally at ../aten/src/ATen/EmptyTensor.cpp:31
         "ignore",
@@ -191,4 +188,9 @@ if __name__ == "__main__":
         "ignore",
         message="UserWarning",
     )
+
+    from jsonargparse import CLI
+
+    torch.set_float32_matmul_precision("high")
+
     CLI(main)
