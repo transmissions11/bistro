767a1d2a6ef8611720dc38ca4164b82fcbb40b8c
t11s <greenfeatherhelp@gmail.com>
2023-08-10 00:29:39 -0600

    Update full.py

--- finetune/full.py
+++ finetune/full.py
@@ -291,6 +291,24 @@ def validate(
                 "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, "
                 "detailed, and polite answers to the user's questions. USER: Generate a game of chess at the Grandmaster level. ASSISTANT: "
             )
+            encoded = torch.cat(
+                (
+                    # todo: dont do this lmeow
+                    torch.tensor(([1] * 20), dtype=torch.int64),
+                    tokenizer.encode(system_prompt, device=model.device),
+                ),
+                dim=0,
+            )
+            max_returned_tokens = len(encoded) + 100
+            output = generate(
+                model,
+                idx=encoded,
+                max_returned_tokens=max_returned_tokens,
+                max_seq_length=max_returned_tokens,
+                temperature=0.7,
+            )
+            output = tokenizer.decode(output)
+            fabric.print(output)
             fabric.print()
 
         logits = model(input_ids)
