b6f393fbd11de7659f3f981ef4ad7f684dd97fda
t11s <greenfeatherhelp@gmail.com>
2023-08-10 14:17:14 -0600

    zz

--- bistro/model.py
+++ bistro/model.py
@@ -111,14 +111,6 @@ class GPT(nn.Module):
 
         # replace the first 20 embeddings of each batch with the soft prompt embeddings
         # todo i think we can just use.weight lol
-        # batch_size = x_pre.size(0)
-        # soft_prompt_output = self.soft_prompt(
-        #     torch.arange(self.num_tokens_in_soft_prompt, device=x_pre.device)
-        # )
-        # soft_prompt_output_batched = soft_prompt_output.unsqueeze(0).expand(
-        #     batch_size, -1, -1
-        # )
-
         x = (
             F.pad(
                 torch.cat(
