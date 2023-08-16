4468acc96d95b1693053b84193e8caa0ec0a08ea
t11s <greenfeatherhelp@gmail.com>
2023-08-16 01:00:30 -0700

    feat: cleanup further

--- bistro/model.py
+++ bistro/model.py
@@ -1,27 +1,28 @@
-from typing import Optional, Tuple, Any
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
-
-from typing_extensions import Self
 
 from lit_gpt.config import Config
 from lit_gpt.rmsnorm import RMSNorm
 from lit_gpt.model import Block, build_rope_cache
 
-RoPECache = Tuple[torch.Tensor, torch.Tensor]
-
 
 class GPT(nn.Module):
     def __init__(self, config: Config, num_tokens_in_soft_prompt) -> None:
         super().__init__()
+
         assert config.padded_vocab_size is not None
         self.config = config
+
+        #############################################################################
 
         self.num_tokens_in_soft_prompt = num_tokens_in_soft_prompt
         self.soft_prompt = nn.Parameter(
             torch.randn(num_tokens_in_soft_prompt, config.n_embd)
         )
+
+        #############################################################################
 
         self.lm_head = nn.Linear(config.n_embd, config.padded_vocab_size, bias=False)
         self.transformer = nn.ModuleDict(
@@ -31,7 +32,8 @@ class GPT(nn.Module):
                 ln_f=config.norm_class(config.n_embd, eps=config.norm_eps),
             )
         )
-        self.rope_cache: Optional[RoPECache] = None
+
+        self.rope_cache: Optional[Tuple[torch.Tensor, torch.Tensor]] = None
 
     def _init_weights(self, module: nn.Module) -> None:
         if isinstance(module, nn.Linear):
@@ -61,17 +63,27 @@ class GPT(nn.Module):
         ), f"Cannot forward sequence of length {T}, block size is only {block_size}"
 
         if self.rope_cache is None:
-            self.rope_cache = self.build_rope_cache(idx)
+            self.rope_cache = build_rope_cache(
+                seq_len=block_size,
+                n_elem=int(self.config.rotary_percentage * self.config.head_size),
+                dtype=torch.get_default_dtype(),
+                device=idx.device,
+                condense_ratio=self.config.condense_ratio,
+            )
 
         cos, sin = self.rope_cache
         cos = cos[:T]
         sin = sin[:T]
 
         # forward the token indexes through the embedding layer
-        x = self.transformer.wte(idx)  # token embeddings of shape (b, t, n_embd)
+        x = self.transformer.wte(idx)  # (b, t, n_embd)
 
-        # replace the first num_tokens_in_soft_prompt embs of each batch with the soft prompt embs
+        #############################################################################
+
+        # swap the first num_tokens_in_soft_prompt embs of each batch for soft_prompt
         x[:, : self.num_tokens_in_soft_prompt] = self.soft_prompt  # (b, t, n_embd)
+
+        #############################################################################
 
         for block in self.transformer.h:
             x, *_ = block(x, (cos, sin), block_size)  # (b, t, n_embd)
@@ -79,16 +91,3 @@ class GPT(nn.Module):
         x = self.transformer.ln_f(x)  # (b, t, n_embd)
 
         return self.lm_head(x)  # (b, t, vocab_size)
-
-    @classmethod
-    def from_name(cls, name: str, **kwargs: Any) -> Self:
-        return cls(Config.from_name(name, **kwargs))
-
-    def build_rope_cache(self, idx: torch.Tensor) -> RoPECache:
-        return build_rope_cache(
-            seq_len=self.config.block_size,
-            n_elem=int(self.config.rotary_percentage * self.config.head_size),
-            dtype=torch.get_default_dtype(),
-            device=idx.device,
-            condense_ratio=self.config.condense_ratio,
-        )
