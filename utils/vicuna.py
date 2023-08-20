bf99599b146676c43d849ae88312e4ba3ac240aa
t11s <greenfeatherhelp@gmail.com>
2023-08-20 03:15:09 -0700

    fix spacing

--- utils/vicuna.py
+++ utils/vicuna.py
@@ -1,10 +1,10 @@
-VICUNA_END_OF_SYSTEM_PROMPT_SEQUENCE = "USER:"
-VICUNA_END_OF_USER_PROMPT_SEQUENCE = "ASSISTANT:"
+VICUNA_END_OF_SYSTEM_PROMPT_SEQUENCE = " USER:"
+VICUNA_END_OF_USER_PROMPT_SEQUENCE = " ASSISTANT:"
 
 
 def fmt_vicuna_input(prompt: str, response: str) -> str:
     return (
         f"A chat between a curious user and an artificial intelligence assistant. "
-        f"The assistant gives helpful, detailed, and polite answers to the user's questions. "
-        f"{VICUNA_END_OF_SYSTEM_PROMPT_SEQUENCE} {prompt} {VICUNA_END_OF_USER_PROMPT_SEQUENCE} {response}"
+        f"The assistant gives helpful, detailed, and polite answers to the user's questions."
+        f"{VICUNA_END_OF_SYSTEM_PROMPT_SEQUENCE} {prompt}{VICUNA_END_OF_USER_PROMPT_SEQUENCE} {response}"
     )
