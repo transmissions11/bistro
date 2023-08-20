68bd4c07494be0a230c6b580b0d1a4e4eceb85d3
t11s <greenfeatherhelp@gmail.com>
2023-08-20 03:53:45 -0700

    try device

--- utils/tensors.py
+++ utils/tensors.py
@@ -7,5 +7,6 @@ def find_subtensor_end(tensor: torch.Tensor, subset: torch.Tensor) -> Optional[i
     """Find the index of the last occurrence of subset in seq."""
 
     windows = tensor.unfold(0, len(subset), 1)
+    print(windows.device, subset.device, tensor.device)
     idx = torch.where((windows == subset).all(dim=1))[0]
     return (idx[0] + len(subset) - 1).item() if idx.numel() else None
