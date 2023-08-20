673840cf6c54a51b4dc8c635f638e9f77f28b937
t11s <greenfeatherhelp@gmail.com>
2023-08-20 03:49:30 -0700

    fix

--- fit.py
+++ fit.py
@@ -182,7 +182,7 @@ def validate(
             target = strip_right_pad(targets[0])
 
             prompt_end_idx = find_subtensor_end(
-                sample, VICUNA_END_OF_USER_PROMPT_SEQUENCE
+                sample, tokenizer.encode(VICUNA_END_OF_USER_PROMPT_SEQUENCE)
             )
 
             print(f"Input: {tokenizer.decode(sample[:prompt_end_idx + 1])}")
