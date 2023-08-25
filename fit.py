067ef11549c9e80d34ffac3754afbf99f36c29fb
t11s <greenfeatherhelp@gmail.com>
2023-08-24 20:48:29 -0700

    16-mixed

--- fit.py
+++ fit.py
@@ -282,7 +282,7 @@ def setup(
     checkpoint_dir: Path = Path("checkpoints/lmsys/vicuna-7b-v1.5"),
     out_dir: Path = Path("out/full/chess"),
     # TODO: Try "transformer-engine" (https://github.com/Lightning-AI/lightning/pull/17597)
-    precision: str = "16-true",
+    precision: str = "16-mixed",
 ):
     if devices > 1:
         strategy = FSDPStrategy(
