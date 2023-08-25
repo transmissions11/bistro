21a77056176cb50793f6060d5e1d01bd5f5ee2fe
t11s <greenfeatherhelp@gmail.com>
2023-08-24 21:00:52 -0700

    Update fit.py

--- fit.py
+++ fit.py
@@ -282,8 +282,8 @@ def setup(
     checkpoint_dir: Path = Path("checkpoints/lmsys/vicuna-7b-v1.5"),
     out_dir: Path = Path("out/full/chess"),
     # TODO: Try "transformer-engine" (https://github.com/Lightning-AI/lightning/pull/17597)
-    # TODO: Make this a W&B sweep param.
-    precision: str = "16-mixed",
+    # TODO: Make this a W&B sweep param (bf16-true, bf16-mixed, 16-true, 16-mixed, fp8, 64, 32)
+    precision: str = "bf16-mixed",
 ):
     if devices > 1:
         strategy = FSDPStrategy(
