87556b29a584393bf393cc3e7a232d03ec96d91e
t11s <greenfeatherhelp@gmail.com>
2023-09-04 17:32:15 -0700

    try num_proc=64

--- lit_datamodule.py
+++ lit_datamodule.py
@@ -53,7 +53,7 @@ class LitDataModule(L.LightningDataModule):
 
         return load_dataset("parquet", data_dir=self.data_dir).map(
             transform,
-            num_proc=8,
+            num_proc=64,
         )
 
     def prepare_data(self):
