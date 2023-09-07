3d24f7342f0e278d1167f1d15a100fe0ee1659b4
t11s <greenfeatherhelp@gmail.com>
2023-09-07 11:34:48 -0700

    start trainer refactor

--- fit.py
+++ fit.py
@@ -57,177 +57,27 @@ hparams = {
 }
 
 
-def train(
-    fabric: L.Fabric,
-    model: LitModel,
-    optimizer: torch.optim.Optimizer,
-    datamodule: LitDataModule,
-    tokenizer: Tokenizer,
-    out_dir: Path,
-    speed_monitor: SpeedMonitor,
-) -> None:
-    # TODO: oh this just only prints if rank 0
-    fabric.print(
-        f"starting val loss: {validate(fabric, model, datamodule, tokenizer):.4f}"
-    )
-
-    measured_flops = 0.0  # TODO: Should get this working again.
-    # with torch.device("meta"):
-    #     meta_model = GPT(model.config, num_tokens_in_soft_prompt)
-    #     x = torch.randint(0, 1, (micro_batch_size, model.config.block_size))
-    #     measured_flops = measure_flops(meta_model, x)
-    #     fabric.print(
-    #         f"Measured TFLOPs: {measured_flops * fabric.world_size / 1e12:.2f}"
-    #     )
-    #     del meta_model, x
-
-    step_count = 0
-    total_lengths = 0
-    total_t0 = time.time()
-
-    # TODO: remove eventually
-    train_dataloader = datamodule.train_dataloader()
-
-    for iter_num, batch in enumerate(train_dataloader):
-        # Linear warmup stage.
-        if step_count <= warmup_steps:
-            lr = learning_rate * step_count / warmup_steps
-            for param_group in optimizer.param_groups:
-                param_group["lr"] = lr
-
-        iter_t0 = time.time()
-
-        is_accumulating = (iter_num + 1) % gradient_accumulation_iters != 0
-
-        with fabric.no_backward_sync(model, enabled=is_accumulating):
-            # TODO: Ideally moving to device gets done for us automatically!
-            # TODO: Idk about iter_num being dataloader index.
-            batch = datamodule.transfer_batch_to_device(batch, fabric.device, iter_num)
-            loss = model.training_step(batch, iter_num)
-
-            fabric.backward(loss / gradient_accumulation_iters)
-
-        if not is_accumulating:
-            optimizer.step()
-            optimizer.zero_grad()
-            step_count += 1
-
-        t1 = time.time()
-        total_lengths += batch["input_ids"].size(1)
-        speed_monitor.on_train_batch_end(
-            (iter_num + 1) * micro_batch_size,
-            t1 - total_t0,
-            # Assumes that device FLOPs are the same
-            # and all devices have the same batch size.
-            fabric.world_size,
-            flops_per_batch=measured_flops,
-            lengths=total_lengths,
-        )
-        if iter_num % log_interval == 0:
-            fabric.log("train/loss", loss.item())
-            fabric.print(
-                f"iter {iter_num} step {step_count}: loss {loss.item():.4f}, iter time:"
-                f" {(t1 - iter_t0) * 1000:.2f}ms{' (optimizer.step)' if not is_accumulating else ''}"
-            )
-
-        if not is_accumulating and step_count % eval_interval == 0:
-            t0 = time.time()
-            val_loss = validate(fabric, model, datamodule, tokenizer)
-            t1 = time.time() - t0
-            speed_monitor.eval_end(t1)
-            # TODO: W&B table to show examples.
-            fabric.log("val/loss", val_loss)
-            fabric.print(
-                f"step {iter_num}: val loss {val_loss:.4f}, val time: {t1 * 1000:.2f}ms"
-            )
-            fabric.barrier()
-
-        if not is_accumulating and step_count % save_interval == 0:
-            save_checkpoint(fabric, model, out_dir / f"iter-{iter_num:06d}-ckpt.pth")
-
-
-@torch.no_grad()
-def validate(
-    fabric: L.Fabric,
-    model: LitModel,
-    # TODO: Should just specify val_dataloader here
-    # but need to manually call transfer_batch_to_device
-    # since we don't have trainer.
-    datamodule: LitDataModule,
-    tokenizer: Tokenizer,
-) -> torch.Tensor:
-    fabric.print("Validating ...")
-
-    model.eval()
-
-    losses = torch.zeros(eval_iters)
-
-    val_dataloader = datamodule.val_dataloader()  # TODO: Remove after fixing the above.
-
-    for k, batch in enumerate(val_dataloader):
-        if k >= eval_iters:
-            break
-
-        # TODO: Ideally moving to device gets done for us automatically!
-        # TODO: Idk about k being dataloader index.
-        batch = datamodule.transfer_batch_to_device(batch, fabric.device, k)
-        loss = model.validation_step(batch, k)
-        losses[k] = loss.item()
-
-        # Target generating 5 examples.
-        if k % (eval_iters // 5) == 0:
-            tokens_out = 10
-
-            sample = strip_right_pad(batch["input_ids"][0])
-            target = strip_right_pad(batch["targets"][0])
-
-            prompt_end_idx = find_subtensor_end(
-                sample,
-                tokenizer.encode(
-                    # TODO: Why did I have to do device=fabric.device here, and not in other places?
-                    VICUNA_END_OF_USER_PROMPT_SEQUENCE,
-                    device=fabric.device,
-                ),
-            )
-
-            # TODO: Print and compare to a baseline w/o the soft prompt. (do we need a flag in the forward pass?)
-
-            # TODO: use fabric.print
-            print(f"Input: {tokenizer.decode(sample[:prompt_end_idx + 1])}")
-            output = sample_model(
-                model,
-                idx=sample[: prompt_end_idx + 1],
-                max_new_tokens=10,
-                temperature=0.01,
-            )[-tokens_out:]
-            print(f"Output:", tokenizer.decode(output))
-            target[target == -1] = 0  # TODO: Just show the relevant targets.
-            print(f"Target:", tokenizer.decode(target))
-            print("\n\n")
-
-    model.train()
-
-    return losses.mean().item()
-
-
-def main(fabric: L.Fabric, data_dir: Path, checkpoint_dir: Path, out_dir: Path):
-    fabric.print("Hyperparams:", hparams)
+def main(data_dir: Path, checkpoint_dir: Path, out_dir: Path):
+    print("Hyperparams:", hparams)
 
     check_valid_checkpoint_dir(checkpoint_dir)
 
-    speed_monitor = SpeedMonitor(fabric, window_size=50, time_unit="seconds")
+    # speed_monitor = SpeedMonitor(fabric, window_size=50, time_unit="seconds")
 
-    fabric.seed_everything(1337)  # Same seed for every process to init model (FSDP)
+    # fabric.seed_everything(1337)  # Same seed for every process to init model (FSDP)
 
-    if fabric.global_rank == 0:
-        os.makedirs(out_dir, exist_ok=True)
+    # i f fabric.global_rank == 0:
+    #     os.makedirs(out_dir, exist_ok=True)
 
     tokenizer = Tokenizer(checkpoint_dir)
 
     config = Config.from_name(name=checkpoint_dir.name)
     checkpoint_path = checkpoint_dir / "lit_model.pth"
-    fabric.print(f"Loading model {str(checkpoint_path)!r} with {config.__dict__}...")
-    with fabric.init_module(empty_init=False):
+    # fabric.print(f"Loading model {str(checkpoint_path)!r} with {config.__dict__}...")
+
+    trainer = L.Trainer()
+
+    with trainer.init_module(empty_init=False):
         gpt = GPT(
             config,
             soft_prompt_tkn=tokenizer.token_to_id(soft_prompt_tkn),
@@ -245,15 +95,14 @@ def main(fabric: L.Fabric, data_dir: Path, checkpoint_dir: Path, out_dir: Path):
     #################################################################
 
     param_breakdown = get_param_breakdown(model)
-    fabric.print(
+    print(
         f"Number of trainable and non-trainable parameters: "
         f"{param_breakdown['num_trainable_params']} | {param_breakdown['num_non_trainable_params']}"
     )
-    fabric.print(f"Trainable parameters: {param_breakdown['trainable_param_names']}")
+    print(f"Trainable parameters: {param_breakdown['trainable_param_names']}")
 
-    optimizer = model.configure_optimizers()
-
-    model, optimizer = fabric.setup(model, optimizer)
+    # optimizer = model.configure_optimizers()
+    # model, optimizer = fabric.setup(model, optimizer)
 
     ################################################################
 
@@ -270,19 +119,21 @@ def main(fabric: L.Fabric, data_dir: Path, checkpoint_dir: Path, out_dir: Path):
 
     ################################################################
 
-    fabric.seed_everything(1337 + fabric.global_rank)
+    # fabric.seed_everything(1337 + fabric.global_rank)
 
-    train(
-        fabric,
-        model,
-        optimizer,
-        datamodule,
-        tokenizer,
-        out_dir,
-        speed_monitor,
-    )
+    # train(
+    #     fabric,
+    #     model,
+    #     optimizer,
+    #     datamodule,
+    #     tokenizer,
+    #     out_dir,
+    #     speed_monitor,
+    # )
 
-    save_checkpoint(fabric, model, out_dir / "bistro_model_finetuned.pth")
+    trainer.fit(model, datamodule=datamodule)
+
+    # save_checkpoint(fabric, model, out_dir / "bistro_model_finetuned.pth")
 
 
 def setup(
@@ -306,14 +157,16 @@ def setup(
     else:
         strategy = "auto"
 
-    fabric = L.Fabric(
-        devices=devices,
-        strategy=strategy,
-        precision=precision,
-        loggers=WandbLogger(project="bistro"),
-    )
+    # fabric = L.Fabric(
+    #     devices=devices,
+    #     strategy=strategy,
+    #     precision=precision,
+    #     loggers=WandbLogger(project="bistro"),
+    # )
 
-    fabric.launch(main, data_dir, checkpoint_dir, out_dir)
+    # fabric.launch(main, data_dir, checkpoint_dir, out_dir)
+
+    main(data_dir, checkpoint_dir, out_dir)
 
 
 if __name__ == "__main__":
