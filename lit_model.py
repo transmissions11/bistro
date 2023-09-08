9debb66afe6ad103633dc0a0bfc0a1dc5d30d4df
t11s <greenfeatherhelp@gmail.com>
2023-09-08 12:56:09 -0700

    try decay

--- lit_model.py
+++ lit_model.py
@@ -8,6 +8,26 @@ from lit_gpt.utils import (
     chunked_cross_entropy,
 )
 
+micro_batch_size = 1  # TODO: Set a larger value for this.
+
+devices = 1
+
+gradient_accumulation_iters = 3
+
+epoch_size = 10_000_000  # TODO: Set this based on the actual dataset dynamically.
+num_epochs = 4
+
+max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
+weight_decay = 0.02  # TODO: Should we be using this for finetuning?
+warmup_steps = (
+    2 * (epoch_size // micro_batch_size) // devices // gradient_accumulation_iters
+)
+
+# TODO: Better config system.
+# TODO: Cosine learning rate scheduler.
+learning_rate = 1
+weight_decay = 1e-2
+
 
 class LitModel(L.LightningModule):
     def __init__(
@@ -26,6 +46,13 @@ class LitModel(L.LightningModule):
     def training_step(self, batch: dict, batch_idx):
         input_ids, targets = batch["input_ids"], batch["targets"]
 
+        print(batch_idx)
+
+        if batch_idx <= warmup_steps:
+            lr = learning_rate * batch_idx / warmup_steps
+            for param_group in self.optimizers[0].param_groups:
+                param_group["lr"] = lr
+
         logits = self.model(input_ids)
         loss = chunked_cross_entropy(logits, targets, chunk_size=0)
 
@@ -43,11 +70,6 @@ class LitModel(L.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        # TODO: Better config system.
-        # TODO: Cosine learning rate scheduler.
-        learning_rate = 0.3
-        weight_decay = 1e-2
-
         return torch.optim.AdamW(
             self.parameters(), lr=learning_rate, weight_decay=weight_decay
         )
