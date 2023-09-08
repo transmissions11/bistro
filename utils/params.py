85d647bc95fd11fa4cc2f45a9624ddee06b877d4
t11s <greenfeatherhelp@gmail.com>
2023-09-08 12:46:39 -0700

    Update params.py

--- utils/params.py
+++ utils/params.py
@@ -1,3 +1,6 @@
+import torch.nn as nn
+
+
 def mark_only_soft_prompt_as_trainable(model: nn.Module) -> None:
     """Sets `requires_grad=False` for all non-soft-prompt weights."""
     for name, param in model.named_parameters():
