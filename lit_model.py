6b6505b52a56ff270357c09d732933fdace0b4f4
t11s <greenfeatherhelp@gmail.com>
2023-09-12 23:32:40 -0700

    Revert "log reduce_fx"
    This reverts commit 207117378dabd3e4d516f2f9de742a15c5db9861.

--- lit_model.py
+++ lit_model.py
@@ -45,11 +45,7 @@ class LitModel(L.LightningModule):
         input_ids, targets = batch["input_ids"], batch["targets"]
         loss = self.compute_loss(input_ids, targets)
 
-        def reduce_fx(x):
-            print(x.shape, x)
-            return torch.mean(x)
-
-        self.log("val/loss", loss, reduce_fx=reduce_fx)
+        self.log("val/loss", loss)
 
         print(batch_idx, loss)
 
