ed4c7e5b0145287f4abf95f75890e314b292abb6
t11s <greenfeatherhelp@gmail.com>
2023-09-16 20:27:01 -0700

    lets try num_proc=8

--- lit_datamodule.py
+++ lit_datamodule.py
@@ -59,7 +59,7 @@ class LitDataModule(L.LightningDataModule):
                 transform,
                 remove_columns=["prompt", "response"],
                 load_from_cache_file=False,  # TODO: Fix this.
-                num_proc=2,
+                num_proc=8,
                 # We can force cache like this if needed.
                 # new_fingerprint="t1"
             )
