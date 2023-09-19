52a82afd0bd7eb935656403eb75251a32d5d040f
t11s <greenfeatherhelp@gmail.com>
2023-09-19 01:32:16 -0700

    reset rope cache

--- fit.py
+++ fit.py
@@ -117,6 +117,9 @@ def main(data_dir: Path, checkpoint_dir: Path):
     wandb_logger.watch(model)
 
     trainer.validate(model, datamodule=datamodule)
+
+    model.model.rope_cache = None
+
     trainer.fit(model, datamodule=datamodule)
 
 
