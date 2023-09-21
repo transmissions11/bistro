7e7c4303aa8b6ea6653832f96e196db4d4f9b08d
t11s <greenfeatherhelp@gmail.com>
2023-09-21 15:02:37 -0700

    hardcode max length

--- utils/padding.py
+++ utils/padding.py
@@ -27,7 +27,7 @@ def pad_collate_fn(batch: List[Dict[str, torch.Tensor]]):
 
     # Find the maximum length of 'input_ids' in the batch
     # TODO: better way to do this? can we do it automatically
-    max_len = max([len(item["input_ids"]) for item in batch])
+    max_len = 500
 
     # Pad 'input_ids' and 'targets' in each item in the batch
     input_ids = [pad_right(item["input_ids"], pad_tkn, max_len) for item in batch]
