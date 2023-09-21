b753ae1ef36223b03e41210ef17ec2f584a7def4
t11s <greenfeatherhelp@gmail.com>
2023-09-21 15:05:58 -0700

    Update padding.py

--- utils/padding.py
+++ utils/padding.py
@@ -7,7 +7,6 @@ from typing import List, Dict
 ignored_tkn, pad_tkn = -1, 0  # Some special tokens.
 
 
-# TODO: Should we pad to a power of 8? (https://github.com/Lightning-AI/lit-gpt/pull/123/files)
 def pad_right(x: torch.Tensor, pad_id: int, pad_to: int) -> torch.Tensor:
     # TODO: should we be using len(x) here?
     return torch.nn.functional.pad(
@@ -21,13 +20,15 @@ def strip_right_pad(tensor):
     ]
 
 
+# TODO: can we get the batch inputs to be in a better format that requires less work? wtf, look at HF pytorch lit example
 # TODO: Detemrine x's type
 def pad_collate_fn(batch: List[Dict[str, torch.Tensor]]):
     print("START batch: ", batch)
 
     # Find the maximum length of 'input_ids' in the batch
     # TODO: better way to do this? can we do it automatically
-    max_len = max([len(item["input_ids"]) for item in batch]) + 10
+    # TODO: Should we pad to a power of 8? (https://github.com/Lightning-AI/lit-gpt/pull/123/files)
+    max_len = max([len(item["input_ids"]) for item in batch])
 
     # Pad 'input_ids' and 'targets' in each item in the batch
     input_ids = [pad_right(item["input_ids"], pad_tkn, max_len) for item in batch]
