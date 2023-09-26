6faf2bd6910da0995803e3ae227a3e2c2157bf91
t11s <greenfeatherhelp@gmail.com>
2023-09-26 11:39:30 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -15,7 +15,7 @@ def main(*, checkpoint: Path, temperature: float = 0.7, interactive: bool = Fals
 
     ckpt = torch.load(str(checkpoint), mmap=True)
 
-    print(ckpt["hyper_parameters"])
+    print(ckpt["hyper_parameters"]["tokenizer"])
 
     # model = GPT(
     #     config=self.hparams.model_config,
