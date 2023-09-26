03bf92550c91920153ff59c2bf350eb06fbf19ce
t11s <greenfeatherhelp@gmail.com>
2023-09-25 22:54:44 -0700

    undo for a brother

--- lit_datamodule.py
+++ lit_datamodule.py
@@ -95,7 +95,7 @@ class LitDataModule(L.LightningDataModule):
             collate_fn=pad_collate_fn,
             # Since we're not computing and storing gradients
             # while validating, we can use a larger batch size.
-            batch_size=self.micro_batch_size * 2,
+            batch_size=self.micro_batch_size * 1,
             num_workers=8,
             pin_memory=True,
         )
