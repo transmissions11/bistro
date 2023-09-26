28592b2c1924e127c0f94db34ab078fec11e9dbc
t11s <greenfeatherhelp@gmail.com>
2023-09-26 12:25:04 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -44,14 +44,16 @@ def main(*, checkpoint: Path, temperature: float = 0.7, interactive: bool = Fals
 
     if not interactive:
         print(
-            inference_model(
-                model,
-                idx=tokenizer.encode(
-                    "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: ✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅ 737 * 850 = ASSISTANT:",
-                    device=device,
-                ),
-                temperature=temperature,
-                max_new_tokens=69,
+            tokenizer.decode(
+                inference_model(
+                    model,
+                    idx=tokenizer.encode(
+                        "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: ✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅ 737 * 850 = ASSISTANT:",
+                        device=device,
+                    ),
+                    temperature=temperature,
+                    max_new_tokens=69,
+                )
             )
         )
 
