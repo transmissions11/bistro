89bf9c40ce10ef8773af0987adaec953774f6386
t11s <greenfeatherhelp@gmail.com>
2023-10-05 09:07:45 -0700

    device

--- lit_model.py
+++ lit_model.py
@@ -43,7 +43,8 @@ class LitModel(L.LightningModule):
         self.save_hyperparameters(ignore=["checkpoint_path"], logger=False)
 
         self.current_hard_prompt = tokenizer.encode(
-            "Please multiply these two 3 digit numbers as best you possibly can. No talk; just go."
+            "Please multiply these two 3 digit numbers as best you possibly can. No talk; just go.",
+            device=self.device,
         )
 
         assert (
