9d7c4640d9840b53dfe7d4167e658a801f4c3edd
t11s <greenfeatherhelp@gmail.com>
2023-10-06 09:20:52 -0700

    Update hard_prompting.py

--- utils/hard_prompting.py
+++ utils/hard_prompting.py
@@ -2,19 +2,25 @@ import torch
 
 from typing import Optional
 
+from lit_gpt import Tokenizer
+
 from utils.loss import compute_loss
 
 from model import GPT
 
 
-def token_gradients(
+def get_hard_prompt_gradients(
     model: GPT,
     *,  # Force keyword arguments.
-    hard_prompt_tkn: int,
     current_hard_prompt: torch.Tensor,  # (num_hard_prompt_tkns)
+    hard_prompt_tkn: int,
     input_ids: torch.Tensor,  # (b = 1, t)
     target_ids: torch.Tensor,  # (b = 1, t)
-):
+) -> torch.Tensor:
+    """
+    Calculates the gradients of the hard prompt with respect to the loss.
+    """
+
     input_ids = input_ids.squeeze(0)  # (t)
 
     embed_weights = model.transformer.wte.weight  # (vocab_size, emb_dim)
@@ -70,36 +76,83 @@ def token_gradients(
     return grad / grad.norm(dim=-1, keepdim=True)
 
 
-def sample_hard_prompt(
-    hard_prompt_tkns: torch.Tensor,
-    grad: torch.Tensor,
+def create_hard_prompt_candidates(
+    current_hard_prompt: torch.Tensor,  # (num_hard_prompt_tkns)
+    hard_prompt_grads: torch.Tensor,  # (num_hard_prompt_tkns, vocab_size)
+    *,  # Force keyword arguments.
     batch_size: int,
     topk: int = 256,
+    # Can be used to use only ASCII tokens, for example.
     not_allowed_tokens: Optional[torch.Tensor] = None,
-):
+) -> torch.Tensor:
+    """
+    Creates a batch of hard prompt candidates by sampling randomly from the top-k tokens.
+
+    The top-k tokens are determined by the hard prompt gradients. Should be used with
+    filter_hard_prompt_candidates to ensure the length of the candidates doesn't explode.
+    """
+
+    # Set the gradients of not allowed tokens to infinity.
     if not_allowed_tokens is not None:
-        grad[:, not_allowed_tokens.to(grad.device)] = float("inf")
+        hard_prompt_grads[:, not_allowed_tokens] = float("inf")
 
-    top_indices = (-grad).topk(topk, dim=1).indices
-    hard_prompt_tkns = hard_prompt_tkns.to(grad.device)
+    # Get the ids of the top-k tokens that would most decrease the loss.
+    top_indices = (-hard_prompt_grads).topk(topk, dim=1).indices
 
-    original_hard_prompt_tkns = hard_prompt_tkns.repeat(batch_size, 1)
+    # Initialize the batch of candidates by just repeating the current hard prompt.
+    candidates_batch = current_hard_prompt.repeat(batch_size, 1)
 
+    # Generate positions for new tokens in the hard prompt by creating a tensor of evenly spaced values.
     new_token_pos = torch.arange(
-        0, len(hard_prompt_tkns), len(hard_prompt_tkns) / batch_size, device=grad.device
-    ).type(torch.int64)
+        0,
+        len(current_hard_prompt),
+        len(current_hard_prompt) / batch_size,
+        device=hard_prompt_grads.device,
+        dtype=torch.int64,
+    )
 
+    # TODO: ADD A COMMENT AFTER PRINTING THIS
+    print(new_token_pos)  # TODO: ADD A COMMENT AFTER PRINTING THIS
+    # TODO: ADD A COMMENT AFTER PRINTING THIS
+
+    # TODO comment
+    # For each new token position, select a random token from the top-k tokens.
     new_token_val = torch.gather(
         top_indices[new_token_pos],
         1,
-        torch.randint(0, topk, (batch_size, 1), device=grad.device),
+        # Randomly select a token from the top-k tokens.
+        torch.randint(0, topk, (batch_size, 1), device=hard_prompt_grads.device),
     )
 
-    new_hard_prompt_tkns = original_hard_prompt_tkns.scatter_(
-        1, new_token_pos.unsqueeze(-1), new_token_val
+    # TODO comment
+    # Return the original hard prompt tokens at the new token positions with the new token values.
+    return candidates_batch.scatter_(1, new_token_pos.unsqueeze(-1), new_token_val)
+
+
+def filter_hard_prompt_candidates(
+    tokenizer: Tokenizer,
+    *,  # Force keyword arguments.
+    current_hard_prompt: torch.Tensor,  # (num_hard_prompt_tkns)
+    hard_prompt_candidates: torch.Tensor,  # (batch_size, num_hard_prompt_tkns)
+) -> torch.Tensor:
+    """
+    Filters candidates that don't match the current hard prompt's length.
+
+    This is due to the non-invertibility of tokenizers, where
+    encode(decode(tkns)) might yield a different tokenization.
+    """
+
+    filtered = [
+        candidate
+        for candidate in hard_prompt_candidates
+        # Ensure the candidate is not the same as the current hard prompt.
+        if not torch.equal(candidate, current_hard_prompt)
+        # Ensure the candidate is the same length after decoding and encoding.
+        and candidate.size(0) == tokenizer.encode(tokenizer.decode(candidate)).size(0)
+    ]
+
+    # If the number of filtered candidates is less than the number of hard
+    # prompt candidates, pad the list with the last candidate and return.
+    return torch.stack(
+        filtered + [filtered[-1]] * (len(hard_prompt_candidates) - len(filtered))
     )
-
-    print("SHAPE", new_hard_prompt_tkns.shape)
-    print(f"{batch_size=}, {topk=}, {not_allowed_tokens=}")
-
-    return new_hard_prompt_tkns
