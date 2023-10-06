c52a4ad14f8ed2315f07218b38f92ea8f1aefe19
t11s <greenfeatherhelp@gmail.com>
2023-10-06 09:57:04 -0700

    Update hard_prompting.py

--- utils/hard_prompting.py
+++ utils/hard_prompting.py
@@ -101,7 +101,7 @@ def create_hard_prompt_candidates(
 
     candidates_batch = current_hard_prompt.repeat(batch_size, 1)
 
-    # Generate a 1d tensor of an index to replace in each row of the batch.
+    # Generate a (batch_size) tensor of an index to replace in each row of the batch.
     new_token_pos = torch.arange(
         0,
         len(current_hard_prompt),
@@ -109,14 +109,14 @@ def create_hard_prompt_candidates(
         device=hard_prompt_grads.device,
     ).type(torch.int64)
 
+    # Generate a (batch_size, 1) tensor of tokens to replace each new_token_pos index with.
     new_token_val = torch.gather(
         top_indices[new_token_pos],
         1,
         torch.randint(0, topk, (batch_size, 1), device=hard_prompt_grads.device),
     )
 
-    print("new token val", new_token_val.shape, new_token_val)
-
+    # Replace the new_token_pos index in each row of the batch with the new_token_val.
     new_hard_prompt_tkns = candidates_batch.scatter_(
         1, new_token_pos.unsqueeze(-1), new_token_val
     )
