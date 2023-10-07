e7556878e2ab149e5599456c9078c9bd94739e21
t11s <greenfeatherhelp@gmail.com>
2023-10-06 18:57:24 -0700

    try a dataset of just 1 problem 1m times

--- train.py
+++ train.py
@@ -76,10 +76,6 @@ def main(
         profiler=profiler,
         deterministic="warn",
         precision=precision,
-        #########################
-        limit_train_batches=1,
-        limit_val_batches=0,
-        ##########################
         val_check_interval=val_check_interval,
         log_every_n_steps=log_every_n_steps,
         enable_checkpointing=False,
