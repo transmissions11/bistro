b87f4584dd77ef0d8d060a2ce06ed2a6aa2f1993
t11s <greenfeatherhelp@gmail.com>
2023-10-06 19:00:57 -0700

    reduce -> reduction

--- utils/loss.py
+++ utils/loss.py
@@ -14,12 +14,12 @@ def compute_loss(
     target_ids: torch.Tensor,
     input_ids: Optional[torch.Tensor] = None,
     input_embs: Optional[torch.Tensor] = None,
-    reduce: bool = True,
+    reduction: str = "mean",
 ) -> torch.Tensor:
     logits = model(input_ids=input_ids, input_embs=input_embs)
     return F.cross_entropy(
         logits.view(-1, logits.size(-1)),
         target_ids.view(-1),
         ignore_index=ignored_tkn,
-        reduce=reduce,
+        reduction=reduction,
     )
