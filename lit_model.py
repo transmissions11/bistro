675bb1e59a5a7da66dd52981cd5c3270db7d431d
t11s <greenfeatherhelp@gmail.com>
2023-10-06 19:09:56 -0700

    batch size of 30?

--- lit_model.py
+++ lit_model.py
@@ -76,7 +76,7 @@ class LitModel(L.LightningModule):
         hard_prompt_candidates = create_hard_prompt_candidates(
             current_hard_prompt=self.current_hard_prompt,
             hard_prompt_grads=hard_prompt_grads,
-            batch_size=20,  # TODO: FIND A GOOD VALUE!!!! MAKE THIS CONFIG
+            batch_size=30,  # TODO: FIND A GOOD VALUE!!!! MAKE THIS CONFIG
             # topk=40,
         )
 
