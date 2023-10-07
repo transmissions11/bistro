bd0e216c3bad210464ade0e49f107598f1195766
t11s <greenfeatherhelp@gmail.com>
2023-10-06 22:30:29 -0700

    spaces

--- utils/masking.py
+++ utils/masking.py
@@ -10,6 +10,7 @@ def mask_before_inclusive(
     delimiter: str, seq: torch.Tensor, tokenizer: Tokenizer
 ) -> torch.Tensor:
     """Replace all tokens before delimiter with ignored_tkn."""
+
     idx = find_subtensor_end(seq, tokenizer.encode(delimiter))
     return torch.cat(
         (
