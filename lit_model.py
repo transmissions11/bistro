608ecfd3065d5c6723f5aae79f6f9cb1965866c9
t11s <greenfeatherhelp@gmail.com>
2023-10-06 20:29:42 -0700

    wait on more

--- lit_model.py
+++ lit_model.py
@@ -75,9 +75,11 @@ class LitModel(L.LightningModule):
         hard_prompt_candidates = create_hard_prompt_candidates(
             current_hard_prompt=self.current_hard_prompt,
             hard_prompt_grads=hard_prompt_grads,
-            batch_size=100,  # TODO: FIND A GOOD VALUE!!!! MAKE THIS CONFIG
-            topk=128,
+            batch_size=50,  # TODO: FIND A GOOD VALUE!!!! MAKE THIS CONFIG
+            topk=1,
         )
+
+        print("CANDIDATES", hard_prompt_candidates.sum(dim=-1))
 
         hard_prompt_candidates = clean_hard_prompt_candidates(
             self.hparams.tokenizer,
