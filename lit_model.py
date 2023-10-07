3d701d123d20e892919936d8ae97bb39a6a9f814
t11s <greenfeatherhelp@gmail.com>
2023-10-07 12:42:04 -0700

    Update lit_model.py

--- lit_model.py
+++ lit_model.py
@@ -112,9 +112,7 @@ class LitModel(L.LightningModule):
         self.log("train_loss", min_loss, prog_bar=True)
 
         if batch_idx % 20 == 0:
-            self.print(
-                "PROMPT", self.hparams.tokenizer.decode(self.current_hard_prompt)
-            )
+            print("PROMPT", self.hparams.tokenizer.decode(self.current_hard_prompt))
 
     def validation_step(self, batch: dict, batch_idx: int) -> None:
         # inputs, targets = batch["inputs"], batch["targets"]
