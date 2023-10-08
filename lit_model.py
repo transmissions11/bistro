976a6db378f81454309048a12ecc07d043cd3fbd
t11s <greenfeatherhelp@gmail.com>
2023-10-07 17:09:05 -0700

    80

--- lit_model.py
+++ lit_model.py
@@ -109,7 +109,7 @@ class LitModel(L.LightningModule):
             hard_prompt_candidates = create_hard_prompt_candidates(
                 current_hard_prompt=self.current_hard_prompt,
                 hard_prompt_grads=hard_prompt_grads,
-                batch_size=88,  # TODO: FIND A GOOD VALUE!!!! MAKE THIS CONFIG
+                batch_size=80,  # TODO: FIND A GOOD VALUE!!!! MAKE THIS CONFIG
                 not_allowed_tokens=self.not_allowed_tokens,
                 topk=10,
             )
