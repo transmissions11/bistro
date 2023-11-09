611b1836f2e9d24deac4651e210c1909c77cceb0
t11s <greenfeatherhelp@gmail.com>
2023-11-09 09:21:23 -0800

    Update hard_prompting.py

--- utils/hard_prompting.py
+++ utils/hard_prompting.py
@@ -111,11 +111,11 @@ def create_hard_prompt_candidates(
     if not_allowed_tokens is not None:
         hard_prompt_grads[:, not_allowed_tokens] = float("inf")
 
+    if debug:
+        print(not_allowed_tokens)
+
     # Get the ids of the top-k tokens that would most decrease the loss.
     top_indices = (-hard_prompt_grads).topk(topk, dim=1).indices
-
-    if debug:
-        print(top_indices)
 
     candidates_batch = current_hard_prompt.repeat(batch_size, 1)
 
