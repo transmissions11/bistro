d92cdabe10de771df8f166effc5d68214b95ba8c
t11s <greenfeatherhelp@gmail.com>
2023-11-08 22:51:26 -0800

    undo rand ints bs

--- utils/hard_prompting.py
+++ utils/hard_prompting.py
@@ -123,15 +123,11 @@ def create_hard_prompt_candidates(
         device=hard_prompt_grads.device,
     ).type(torch.int64)
 
-    rand_ints = torch.randint(0, topk, (batch_size, 1), device=hard_prompt_grads.device)
-
-    print("RAND INTS", rand_ints.type(torch.float32).mean())
-
     # Generate a (batch_size, 1) tensor of token ids to replace each new_token_pos index with.
     new_token_val = torch.gather(
         top_indices[new_token_pos],
         1,
-        rand_ints,
+        torch.randint(0, topk, (batch_size, 1), device=hard_prompt_grads.device),
     )
 
     # Replace the new_token_pos index in each row of the batch with the new_token_val.
