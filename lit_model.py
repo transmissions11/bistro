d25465c7ed5a2201b2a6e125ca5030d352b9a451
t11s <greenfeatherhelp@gmail.com>
2023-11-09 12:03:31 -0800

    float64 all the things

--- lit_model.py
+++ lit_model.py
@@ -87,6 +87,7 @@ class LitModel(L.LightningModule):
             torch.zeros(
                 self.hparams.num_hard_prompt_tkns,
                 self.hparams.tokenizer.vocab_size,
+                dtype=torch.float64,
             ),
             persistent=False,
         )
@@ -129,7 +130,7 @@ class LitModel(L.LightningModule):
             hard_prompt_tkn=self.hparams.hard_prompt_tkn,
             input_ids=inputs,
             target_ids=targets,
-        )
+        ).type(torch.float64)
 
         self.accumulated_grads += current_grads
 
