126af1be927f379f4bd75af4b08e365b545aeb62
t11s <greenfeatherhelp@gmail.com>
2023-11-10 23:57:11 -0800

    bsize 6

--- lit_model.py
+++ lit_model.py
@@ -124,7 +124,7 @@ class LitModel(L.LightningModule):
             hard_prompt_candidates = create_hard_prompt_candidates(
                 current_hard_prompt=self.current_hard_prompt,
                 hard_prompt_grads=grads,
-                batch_size=4,  # TODO: find a good value and make this configurable
+                batch_size=6,  # TODO: find a good value and make this configurable
                 not_allowed_tokens=self.not_allowed_tokens,
                 topk=50,
             )
