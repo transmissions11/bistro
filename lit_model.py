928e2a88c2a1173460150981efb0f5f6b84ad0d1
t11s <greenfeatherhelp@gmail.com>
2023-11-10 22:35:52 -0800

    print cand

--- lit_model.py
+++ lit_model.py
@@ -171,6 +171,11 @@ class LitModel(L.LightningModule):
             )
             self.hard_prompt_step += 1.0
 
+            for i in range(5):
+                self.print(
+                    f"CAND {i}",
+                    self.hparams.tokenizer.decode(hard_prompt_candidates[i]),
+                )
             self.print("CAND LOSSES", candidate_losses)
             if self.hard_prompt_step == 3.0:
                 raise ValueError("DONE")
