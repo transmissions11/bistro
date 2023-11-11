d29ac4a4646cd1998f702d319186e4531e5d61d5
t11s <greenfeatherhelp@gmail.com>
2023-11-10 22:15:51 -0800

    log

--- lit_model.py
+++ lit_model.py
@@ -171,6 +171,10 @@ class LitModel(L.LightningModule):
             )
             self.hard_prompt_step += 1.0
 
+            self.print("CAND LOSSES", candidate_losses)
+            if self.hard_prompt_step == 3.0:
+                raise ValueError("DONE")
+
     def validation_step(self, batch: dict, batch_idx: int) -> None:
         inputs, targets = batch["inputs"], batch["targets"]
 
