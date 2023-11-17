96010386a2308d1a887f780d3ff6bd4ae410fa3b
t11s <greenfeatherhelp@gmail.com>
2023-11-16 23:45:46 -0800

    fix?

--- lit_model.py
+++ lit_model.py
@@ -63,7 +63,8 @@ class LitModel(L.LightningModule):
         self.register_buffer(
             "current_hard_prompt",
             torch.tensor(
-                [hard_prompt_tkn] * num_hard_prompt_tkns,
+                [self.tokenizer.bos_id]
+                + [hard_prompt_tkn] * (num_hard_prompt_tkns - 1),
                 dtype=torch.int64,
             ),
         )
