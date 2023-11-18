ba49e1b26ffddac657336f9c165e14978403d07f
t11s <greenfeatherhelp@gmail.com>
2023-11-18 14:16:36 -0800

    512 topk

--- lit_model.py
+++ lit_model.py
@@ -132,7 +132,7 @@ class LitModel(L.LightningModule):
                 hard_prompt_grads=grads,
                 num_candidates=num_candidates,
                 not_allowed_tokens=self.not_allowed_tokens,
-                topk=50,  # TODO: make configurable
+                topk=512,  # TODO: make configurable
             )
 
             # TODO: make sure cands are all in the same place
