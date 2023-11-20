fix: bos=False in masking.py

utils/masking.py
@@ -11,7 +11,9 @@ def mask_before_inclusive(
 ) -> torch.Tensor:
     """Replace all tokens before delimiter with ignored_tkn."""
 
-    idx = find_subtensor_end(seq, tokenizer.encode(delimiter))
+    # bos/eos=False because the delimiter might be in the middle of the sequence.
+    idx = find_subtensor_end(seq, tokenizer.encode(delimiter, bos=False, eos=False))
+
     return torch.cat(
         (
             torch.full((idx + 1,), ignored_tkn, dtype=seq.dtype),