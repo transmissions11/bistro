53237d6624aab3b95bb4a390581fee0b8b325c6e
t11s <greenfeatherhelp@gmail.com>
2023-11-21 12:08:55 -0800

    Update lit_model.py

--- lit_model.py
+++ lit_model.py
@@ -103,7 +103,11 @@ class LitModel(L.LightningModule):
 
         if batch_idx % self.trainer.log_every_n_steps == 0:
             # If this is a log step, log the current hard prompt.
-            self.print("Current hard prompt: |", self.current_hard_prompt, "|")
+            self.print(
+                "Current hard prompt: |",
+                self.hparams.tokenizer.decode(self.current_hard_prompt),
+                "|",
+            )
 
     def validation_step(self, batch: dict, batch_idx: int) -> None:
         inputs, targets = batch["inputs"], batch["targets"]
