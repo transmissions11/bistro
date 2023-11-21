ddcd867c6b8ee63bf3eae53e06ce703ebc572682
t11s <greenfeatherhelp@gmail.com>
2023-11-20 19:35:50 -0800

    dont manually set bos

--- lit_datamodule.py
+++ lit_datamodule.py
@@ -40,7 +40,6 @@ class LitDataModule(L.LightningDataModule):
                     x["targets"],
                     space_before_prompt=False,  # We don't want a space before the hard prompt tokens.
                 ),
-                bos=True,  # TODO: Don't manually set this, just use the default.
                 eos=True,  # Don't see why you wouldn't want to train with an eos_token.
             )
 
