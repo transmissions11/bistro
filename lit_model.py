8337720498267d4bbefe80ae76ce667247a06523
t11s <greenfeatherhelp@gmail.com>
2024-01-01 16:14:07 -0800

    debugging: pare everything down to just compute_loss

--- lit_model.py
+++ lit_model.py
@@ -9,6 +9,7 @@ from typing import Optional
 
 from lit_gpt import Config, Tokenizer
 
+from utils.loss import compute_loss
 from utils.inference import inference_model
 from utils.tensors import find_subtensor_end
 from utils.curriculum import CurriculumCollate
@@ -73,156 +74,28 @@ class LitModel(L.LightningModule):
     def training_step(self, batch: dict, batch_idx: int) -> torch.Tensor:
         inputs, targets = batch["inputs"], batch["targets"]
 
-        # Ensure the batches being passed in are the right size, in sync with the curriculum.
-        assert self.curriculum_collate.num_learned_samples + 1 == inputs.size(0)
+        ####################################################
 
-        # .stack(...) -> (num_learned_samples + 1, num_hard_prompt_tkns, vocab_size)
-        # .sum(dim=0) -> (num_hard_prompt_tkns, vocab_size)
-        grads = torch.stack(
-            [
-                get_hard_prompt_gradients(
-                    self.model,
-                    current_hard_prompt=self.current_hard_prompt,
-                    hard_prompt_tkn=self.hparams.hard_prompt_tkn,
-                    input_ids=input,
-                    target_ids=target,
-                )  # (num_hard_prompt_tkns, vocab_size)
-                for input, target in zip(inputs, targets)
-            ]
-        ).sum(dim=0)
+        import ipdb
 
-        candidates = create_hard_prompt_candidates(
-            current_hard_prompt=self.current_hard_prompt,
-            hard_prompt_grads=grads,
-            tokenizer=self.hparams.tokenizer,
-            topk=self.hparams.topk,
-            num_candidates=(
-                self.hparams.candidate_batch_size * self.hparams.num_candidate_batches
-            ),
-            not_allowed_tokens=self.not_allowed_tokens,
-        )  # (num_candidates, num_hard_prompt_tkns)
-
-        # .stack(...) -> (num_learned_samples + 1, num_candidates)
-        # .sum(dim=0) -> (num_candidates)
-        candidate_losses = torch.stack(
-            [
-                test_hard_prompt_candidates(
-                    self.model,
-                    candidate_batch_size=self.hparams.candidate_batch_size,
-                    hard_prompt_candidates=candidates,
-                    hard_prompt_tkn=self.hparams.hard_prompt_tkn,
-                    input_ids=input,
-                    target_ids=target,
-                )  # (num_candidates)
-                for input, target in zip(inputs, targets)
-            ]
-        ).mean(dim=0)
-
-        min_loss, min_idx = torch.min(candidate_losses, dim=0)
-
-        self.log("train_loss", min_loss)
-
-        self.current_hard_prompt = candidates[min_idx]  # Update the hard prompt.
-
-        if batch_idx % self.trainer.log_every_n_steps == 0:
-            # TODO: Log the raw ids anywhere? Ideally just log the running best one?
-
-            # If this is a log step, log the current hard prompt.
-            self.print(
-                "Current hard prompt: |"
-                # TODO: Decode and then encode it again with the | around
-                # it to ensure no tokenization weirdness with prefix spaces.
-                + self.hparams.tokenizer.decode(self.current_hard_prompt)
-                + "|",
-            )
-
-        if min_loss <= self.hparams.expansion_loss_threshold:
-            # If the new hard prompt meets the loss threshold for
-            # expanding the curriculum, expand it with a new sample.
-            self.curriculum_collate.expand_curriculum()
-
-            self.print(
-                f"Min Loss ({min_loss}) <= Threshold ({self.hparams.expansion_loss_threshold}), expanded curriculum to {self.curriculum_collate.num_learned_samples + 1} samples."
-            )
-
-        self.log(
-            "num_learned_samples", float(self.curriculum_collate.num_learned_samples)
+        ipdb.set_trace(
+            cond=(0 == torch.distributed.get_rank())
+            if torch.distributed.is_initialized()
+            else True
         )
+
+        ####################################################
+
+        with torch.inference_mode():
+            loss = compute_loss(
+                self.model,
+                input_ids=inputs,
+                target_ids=targets,
+                reduction="none",
+            )
 
     def validation_step(self, batch: dict, batch_idx: int) -> None:
-        # TODO: Actually validate multiple samples instead of just 1.
-
-        inputs, targets = batch["inputs"], batch["targets"]
-
-        candidate_losses = test_hard_prompt_candidates(
-            self.model,
-            # We only have 1 "candidate" to test,
-            # and that's the current hard prompt.
-            candidate_batch_size=1,
-            hard_prompt_candidates=self.current_hard_prompt.unsqueeze(0),
-            hard_prompt_tkn=self.hparams.hard_prompt_tkn,
-            input_ids=inputs,
-            target_ids=targets,
-        )  # (1)
-
-        self.log(
-            "val_loss",
-            candidate_losses.mean(dim=0),
-            on_epoch=True,
-            prog_bar=True,
-            sync_dist=True,
-        )
-
-        # Log a few sample inferences from the validation set to W&B.
-        if batch_idx == 0:
-            tokenizer = self.hparams.tokenizer
-
-            prompt_end_tkns = tokenizer.encode(
-                VICUNA_END_OF_USER_PROMPT_SEQUENCE,
-                device=self.device,
-                # bos/eos=False because the "end tokens"
-                # will be in the middle of the sequence.
-                bos=False,
-                eos=False,
-            )
-
-            def process_val_sample(sample, target):
-                sample = strip_right_pad(sample)
-                target = strip_right_pad(target)
-
-                # Insert the hard prompt into the given input sequence.
-                input_ids = insert_hard_prompt_into_template(
-                    template_input_ids=(
-                        sample[: find_subtensor_end(sample, prompt_end_tkns) + 1]
-                    ),
-                    hard_prompt=self.current_hard_prompt,
-                    hard_prompt_tkn=self.hparams.hard_prompt_tkn,
-                )
-
-                return (
-                    tokenizer.decode(input_ids),
-                    tokenizer.decode(
-                        inference_model(
-                            self.model,
-                            input_ids,
-                            temperature=0.00,  # Sample 100% greedily.
-                            max_new_tokens=100,  # Should hit an eos token first.
-                            eos_id=tokenizer.eos_id,
-                        )
-                    ),
-                    # Note: target != ignored_tkn strips away ignored_tkn tokens entirely,
-                    # which may lead to confusion if ignored_tkn is used between real tokens.
-                    tokenizer.decode(target[target != ignored_tkn]),
-                )
-
-            self.logger.log_text(
-                key="val_samples",
-                columns=["input", "output", "target"],
-                data=[
-                    process_val_sample(inputs[i], targets[i])
-                    for i in range(len(inputs))  # Full batch.
-                ],
-            )
+        pass
 
     def configure_optimizers(self):
         ...  # We don't need an optimizer.
