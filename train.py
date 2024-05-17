a8fe82759bdf71a7d61a39e28e9817026f871f7a
t11s <greenfeatherhelp@gmail.com>
2024-05-17 00:01:13 -0700

    bsize=32, grad=8

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def main(
     devices: int = -1,  # -1 for all available GPUs, 1 for 1 GPU, etc.
     strategy: str = "auto",
     micro_batch_size: int = 32,
-    gradient_accumulation_iters: int = 4,
+    gradient_accumulation_iters: int = 8,
     precision: str = "bf16-true",
     ####################################################################
     max_time: Optional[str] = None,  # Specify with DD:HH:MM:SS format.
