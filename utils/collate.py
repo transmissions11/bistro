92ed65655f79f7b2edae0b9043eb8eee5446de84
t11s <greenfeatherhelp@gmail.com>
2024-05-20 14:17:53 -0700

    debug: just log batch

--- utils/collate.py
+++ utils/collate.py
@@ -2,15 +2,7 @@ import torch
 
 
 def collate_fn(batch):
-    import ipdb
-
-    ipdb.set_trace(
-        cond=(
-            (0 == torch.distributed.get_rank())
-            if torch.distributed.is_initialized()
-            else True
-        )
-    )
+    print(batch)
 
     data = torch.stack([item[0] for item in batch])  # pixel values
     target = torch.stack([item[1] for item in batch])  # labels
